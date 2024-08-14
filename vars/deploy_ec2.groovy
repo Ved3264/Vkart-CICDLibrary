74663386a4d2376a74e7e524895297e3e26f0af4
ved patel <pved81034@gmail.com>
2024-08-14 23:37:20 +0530

    update deploy file

--- vars/deploy_ec2.groovy
+++ vars/deploy_ec2.groovy
@@ -3,6 +3,6 @@ def call()
 {
     def dockerCmd = "docker run -p 8081:8000 -d ${env.IMG_TAG}"
     sshagent(['ec2-shh-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@44.202.81.47 '${dockerCmd}'"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@13.232.157.100 '${dockerCmd}'"
     }
 }
